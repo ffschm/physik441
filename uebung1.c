f6bf75520d252cd6d1afc5ab1aff7e3bc5cc74d3
ffschm <s6ffschm@uni-bonn.de>
2015-05-01 15:36:28 +0200

    Code neu formatiert.

--- uebung1.c
+++ uebung1.c
@@ -25,7 +25,7 @@ double powm1(int i){
 
 
 /* definiere Funktion, die mir die Madlung Konstante errechnet im 2dim Fall */
-double madelung_2dim (int n) {
+double madelung2d (int n) {
 	/* definiere Laufvariablen */
 	int i;
 	int j;
@@ -34,22 +34,13 @@ double madelung_2dim (int n) {
 	for (i=-n; i<=n; i++) {
 		for (j=-n; j<=n; j++) {
 			/* Sonderbehandlung Nullpunkt*/
-			if (i==0 && j==0) {
-				sum = sum;
-			}
-			else {	
-				/*auf der Kante*/		
-				if (abs(i)==n && abs(j)==n) {
-					sum += powm1(i+j)/(4*sqrt(i*i+j*j));
-				}
-				/*auf der Seite*/
-				else if (abs(i)==n || abs(j)==n) {
-					sum += powm1(i+j)/(2*sqrt(i*i+j*j));
-				}
-				/*im Wuerfel*/
-				else {
-					sum += powm1(i+j)/(sqrt(i*i+j*j));
-				}
+			if (i==0 && j==0) {			/* mittleres Ion */
+			} else if (abs(i)==n && abs(j)==n) {	/* auf einer Kante */
+				sum += powm1(i+j)/sqrt(i*i+j*j)/4;
+			} else if (abs(i)==n || abs(j)==n) {	/* auf einer Seite */
+				sum += powm1(i+j)/sqrt(i*i+j*j)/2;
+			} else {				/* im Wuerfel */
+				sum += powm1(i+j)/sqrt(i*i+j*j);
 			}
 		}
 	}
@@ -71,20 +62,18 @@ double madelung3d (int n) {
 		for (j=-n; j<=n; j++) {
 			for (k=-n; k<=n; k++) {
 				if (i==0 && j==0 && k==0) {
+				} else if (abs(i)==n && abs(j)==n && abs(k)==n) {
+					sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k)/8;
+
+				} else if (abs(i)==n || abs(j)==n || abs(k)==n) {
+					sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k)/2;
+
+				} else if ((abs(i)==n && abs(j)==n) || (abs(i)==n && 
+					   abs(k)==n) || (abs(k)==n && abs(j)==n)) {
+					sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k)/4;
+
 				} else {
-					if (abs(i)==n && abs(j)==n && abs(k)==n) {
-						sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k)/8;
-					}
-					else if (abs(i)==n || abs(j)==n || abs(k)==n) {
-						sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k)/2;
-					}
-					else if ((abs(i)==n && abs(j)==n) || (abs(i)==n && 
-							 abs(k)==n) || (abs(k)==n && abs(j)==n)) {
-						sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k)/4;
-					}
-					else {
-						sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k);
-					}
+					sum += powm1(i+j+k)/sqrt(i*i+j*j+k*k);
 				}
 			}
 		}
@@ -99,40 +88,43 @@ double madelung3d (int n) {
  * var - gewünschte Genauigkeit
  */
 double bindungsenergie (int dim, double d, double var) {
-	int n = 1;
-	double E_alt;
-	double E_neu;
+	int n=1;
+	double m_alt;
+	double m_neu;
 
-	/* definiere Konstante k */
-	double k = 1/(4*M_PI*eps_0)*pow(e,2);
+	/* Berechne Vorfaktor k */
+	double k = 1/(4*M_PI*eps_0)*e*e;
+
 	/* Fallunterscheidung fuer 3dim, 2dim */
 	if (dim == 2) {
-		E_alt = madelung_2dim(1)*k/d;
-		E_neu = madelung_2dim(2)*k/d;
-		/* benutze var um Iteration ueber n zu bestimmen (Groesse des Wuerfels) */
-		while (fabs((E_neu)/(E_alt)-1)>var) {
-			n++;
-			E_alt = E_neu;
-			E_neu = madelung_2dim(n)*k/d;
-		}
-	} else {
-		/* benutze var um Iteration ueber n zu bestimmen (Groesse des Wuerfels) */
+		/* siehe unten */
 		do {
-			E_alt = E_neu;
-			E_neu = madelung3d(n)*k/d;
-			printf("E(n = %i) = %.20f\n",n,E_neu);
+			m_alt = m_neu;
+			m_neu = madelung2d(n);
+			printf("E(n = %i) = %.20f\n",n,m_neu);
 			n++;
-		} while (fabs(E_neu/E_alt-1) > var);
+		} while (fabs(m_neu/m_alt-1) > var);
+	} else {
+		/* Iteriere über Kantenlänge n des Würfels,
+		 * bis die Abweihung zwischen E(n) und E(n-1) kleiner als
+		 * die gewünschte Genauigkeit ist.
+		 */
+		do {
+			m_alt = m_neu;
+			m_neu = madelung3d(n);
+			printf("E(n = %i) = %.20f\n",n,m_neu);
+			n++;
+		} while (fabs(m_neu/m_alt-1) > var);
 	}
-	return E_neu;
+	return m_neu*k/d;
 }
 
 
 int main () {
 	/* Genauigkeit */
-	double var = pow(10,-5);
-	long double Gitterkonstante = 5.62*pow(10, -10);
-	int dim = 3;
+	double var = 1E-5;
+	long double Gitterkonstante = 5.62E-10;
+	int dim = 2;
 	double Energie = bindungsenergie(dim, Gitterkonstante, var);
 	/* TODO: Einheit!!*/
 	printf("Bindungsenergie in %iD mit einer Genauigkeit von %E beträgt:\n %E Einheit??\n",dim,var,Energie);
