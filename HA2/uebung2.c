4bda84d22d52f46a4ab748d76845551672bd8ad2
ffschm <s6ffschm@uni-bonn.de>
2015-05-14 00:49:30 +0200

    Korrigiere bei(x).
    Der 0-te Term von bei(x) wurde berichtigt.
    Es existieren nun die Funktionen ber(x,prec) und bei(x,prec), die im gesamten Wertebereich
    den richtigen Wert zurückgeben (Trennstelle ist frei bei x=30 gewählt).

--- HA2/uebung2.c
+++ HA2/uebung2.c
@@ -15,11 +15,11 @@
 
 /* Realteil der Besselfunktion 0-ter Ordnung 
  * Berechnung durch eine Potenzreihenentwicklung */
-double ber0 (double x, double prec) {
+double ber0_ps (double x, double prec) {
 	int k;
 	double t,ber_neu,ber_alt;
 	t = 1;
-	ber_neu = 1;
+	ber_neu = t;
 	k = 0;
 	do {
 		t = -pow(x/2,4)/pow(4*k*k+6*k+2,2)*t;
@@ -35,11 +35,11 @@ double ber0 (double x, double prec) {
 
 /* Imaginärteil der Besselfunktion 0-ter Ordnung
  * Berechnung durch eine Potenzreihenentwicklung */
-double bei0 (double x, double prec) {
+double bei0_ps (double x, double prec) {
 	int k;
 	double t,bei_neu,bei_alt;
-	t = 1;
-	bei_neu = 0;
+	t = pow(x/2,2);
+	bei_neu = t;
 	k = 0;
 	do {
 		t = -pow(x/2,4)/pow(4*k*k+10*k+6,2)*t;
@@ -53,14 +53,6 @@ double bei0 (double x, double prec) {
 }
 
 
-/* Ableitung des Realteils der Besselfunktion 0-ter Ordnung
- * Berechnung durch h-Methode
- * unter Verwendung der Potenzreihendarstellung */
-double derive_ber0(double x, double prec, double h) {
-	return (ber0(x+h,prec)-ber0(x,prec))/h;
-}
-
-
 /* Besselelfunktion 0-ter Ordnung
  * Berechnung durch asymptotische Näherung
  * J0(z) = sqrt(2/(pi*z))*cos(z-pi/4) */
@@ -69,18 +61,61 @@ double complex J0_an (double complex z) {
 }
 
 
+/* Definiere den Realteil der Besselfunktion 0-ter Ordnung.
+ * Nutze dazu eine Fallunterscheidung. */
+double ber0(double x,double prec) {
+	if (x<30) {
+		return ber0_ps(x,prec);
+	} else {
+		return creal(J0_an(x*csqrt(-I)));
+	}
+}
 
-double ber0_test(double x) { return ber0(x,1E-6); }
-double bei0_test(double x) { return bei0(x,1E-6); }
-double derive_ber0_test(double x) { return derive_ber0(x,1E-5,1E-8); }
-double J0_test(double x) { return creal(J0_an(x*csqrt(-I)))/exp(x*M_SQRT1_2); }
+
+/* Definiere den Imaginärteil der Besselfunktion 0-ter Ordnung.
+ * Nutze dazu eine Fallunterscheidung. */
+double bei0(double x,double prec) {
+	if (x<30) {
+		return bei0_ps(x,prec);
+	} else {
+		return cimag(J0_an(x*csqrt(-I)));
+	}
+}
+
+
+/* Ableitung des Realteils der Besselfunktion 0-ter Ordnung
+ * Berechnung durch h-Methode
+ * unter Verwendung der Potenzreihendarstellung */
+double derive_ber0(double x, double prec, double h) {
+	return (ber0(x+h,prec)-ber0(x,prec))/h;
+}
+
+
+/* Ableitung des Imaginärteils der Besselfunktion 0-ter Ordnung
+ * Berechnung durch h-Methode
+ * unter Verwendung der Potenzreihendarstellung */
+double derive_bei0(double x, double prec, double h) {
+	return (bei0(x+h,prec)-bei0(x,prec))/h;
+}
+
+
+/* * * * * * * * Test-Funktionen * * * * * * */
+
+double ber0_small_test(double x) { return ber0(x,1E-6); }
+double bei0_small_test(double x) { return bei0(x,1E-6); }
+double derive_ber0_small_test(double x) { return derive_ber0(x,1E-5,1E-8); }
+double derive_ber0_big_test(double x) { return derive_ber0(x,1E-5,1E-8)/exp(x*M_SQRT1_2); }
+double ber0_big_test(double x) { return ber0(x,1E-6)/exp(x*M_SQRT1_2); }
+double bei0_big_test(double x) { return bei0(x,1E-6)/exp(x*M_SQRT1_2); }
 
 
 void plotIt() {
-	plot(&ber0_test,0,10,1000); /* Plottet ber(x) */
-	plot(&bei0_test,0,10,1000); /* Plottet ber(x) */
-	plot(&derive_ber0_test,0,10,1000); /* Plottet ber'(x) */
-//	plot(&J0_test,0,100,1000);/* Plottet J0(x)/exp(x/sqrt(2)) zwischen 0 und 100 */
+	plot(&ber0_small_test,0,10,1000); /* Plottet ber(x) */
+//	plot(&bei0_small_test,0,10,1000); /* Plottet ber(x) */
+//	plot(&derive_ber0_small_test,0,10,1000); /* Plottet ber'(x) */
+//	plot(&derive_ber0_big_test,0,100,1000); /* Plottet ber'(x) */
+	plot(&ber0_big_test,0,100,1000);/* Plottet J0(x)/exp(x/sqrt(2)) zwischen 0 und 100 */
+//	plot(&bei0_big_test,0,100,1000);/* Plottet J0(x)/exp(x/sqrt(2)) zwischen 0 und 100 */
 }
 
 
