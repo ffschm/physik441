dfd0414d14e0dfe5ea7829de83472b8266cee78d
ffschm <s6ffschm@uni-bonn.de>
2015-05-13 20:29:18 +0200

    Implementiere bei(x) und die Ableitung von ber(x).
    Die Ableitung von ber(x) wird als einfacher Differenzquotient bestimmt.
    bei(x) wird dargestellt als Potenzreihe.

--- HA2/uebung2.c
+++ HA2/uebung2.c
@@ -19,7 +19,7 @@ double ber0 (double x, double prec) {
 	int k;
 	double t,ber_neu,ber_alt;
 	t = 1;
-	ber_neu = 0;
+	ber_neu = 1;
 	k = 0;
 	do {
 		t = -pow(x/2,4)/pow(4*k*k+6*k+2,2)*t;
@@ -32,40 +32,55 @@ double ber0 (double x, double prec) {
 	return ber_neu;
 }
 
+
+/* Imaginärteil der Besselfunktion 0-ter Ordnung
+ * Berechnung durch eine Potenzreihenentwicklung */
+double bei0 (double x, double prec) {
+	int k;
+	double t,bei_neu,bei_alt;
+	t = 1;
+	bei_neu = 0;
+	k = 0;
+	do {
+		t = -pow(x/2,4)/pow(4*k*k+10*k+6,2)*t;
+		bei_alt = bei_neu;
+		bei_neu += t;
+		k++;
+//		printf("%i ber_neu = %.10f\n",k,ber_neu);
+	}while (fabs(bei_neu/bei_alt-1) > prec);
+
+	return bei_neu;
+}
+
+
+/* Ableitung des Realteils der Besselfunktion 0-ter Ordnung
+ * Berechnung durch h-Methode
+ * unter Verwendung der Potenzreihendarstellung */
+double derive_ber0(double x, double prec, double h) {
+	return (ber0(x+h,prec)-ber0(x,prec))/h;
+}
+
+
 /* Besselelfunktion 0-ter Ordnung
- * Berechnung durch asymptotische Näherung */
+ * Berechnung durch asymptotische Näherung
+ * J0(z) = sqrt(2/(pi*z))*cos(z-pi/4) */
 double complex J0_an (double complex z) {
 	return M_SQRT2*csqrt(M_1_PI/z)*ccos(z-M_PI_4);
 }
 
 
-void test_ber() {
-	printf("Berechne ber0(0.1)...\n");
-	printf("bar0_plot (0.1) = %.2f \n" , ber0(0.1,1E-6));
 
-	int i;
-	for (i = 0;i == 0; i++){
-		printf("ber(%.3f) = %.5f\n",i,ber0(i,1E-10));
-	}
-}
-
-
-double ber0_test(double x) {
-	return ber0(x,1E-6);
-}
-
-
-double J0_test(double x) {
-	return J0_an(x);
-}
+double ber0_test(double x) { return ber0(x,1E-6); }
+double bei0_test(double x) { return bei0(x,1E-6); }
+double derive_ber0_test(double x) { return derive_ber0(x,1E-5,1E-8); }
+double J0_test(double x) { return creal(J0_an(x*csqrt(-I)))/exp(x*M_SQRT1_2); }
 
 
 void plotIt() {
-	/* Plottet ber(x) zwischen 0 und 10 */
-//	plot(&ber0_test,0,10,1000);
-
-	/* Plottet J0(x) zwischen 5 und 20 */
-	plot(&J0_test,5,20,1000);
+	plot(&ber0_test,0,10,1000); /* Plottet ber(x) */
+	plot(&bei0_test,0,10,1000); /* Plottet ber(x) */
+	plot(&derive_ber0_test,0,10,1000); /* Plottet ber'(x) */
+//	plot(&J0_test,0,100,1000);/* Plottet J0(x)/exp(x/sqrt(2)) zwischen 0 und 100 */
 }
 
 
