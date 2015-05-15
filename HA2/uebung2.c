5e65e415503205037d0eac406ad5c119b305f588
ffschm <s6ffschm@uni-bonn.de>
2015-05-15 14:37:10 +0200

    2015-05-15-14-37

--- HA2/uebung2.c
+++ HA2/uebung2.c
@@ -13,6 +13,9 @@
 
 #include "plot.h"
 
+const double SI_C = 299792458; /* m/s */
+
+
 /* Realteil der Besselfunktion 0-ter Ordnung 
  * Berechnung durch eine Potenzreihenentwicklung */
 double ber0_ps (double x, double prec) {
@@ -26,7 +29,7 @@ double ber0_ps (double x, double prec) {
 		ber_alt = ber_neu;
 		ber_neu += t;
 		k++;
-//		printf("%i ber_neu = %.10f\n",k,ber_neu);
+/*		printf("%i ber_neu = %.10f\n",k,ber_neu); */
 	}while (fabs(ber_neu/ber_alt-1) > prec);
 
 	return ber_neu;
@@ -46,7 +49,7 @@ double bei0_ps (double x, double prec) {
 		bei_alt = bei_neu;
 		bei_neu += t;
 		k++;
-//		printf("%i ber_neu = %.10f\n",k,ber_neu);
+/*		printf("%i ber_neu = %.10f\n",k,ber_neu); */
 	}while (fabs(bei_neu/bei_alt-1) > prec);
 
 	return bei_neu;
@@ -64,7 +67,7 @@ double complex J0_an (double complex z) {
 /* Definiere den Realteil der Besselfunktion 0-ter Ordnung.
  * Nutze dazu eine Fallunterscheidung. */
 double ber0(double x,double prec) {
-	if (x<30) {
+	if (x<10) {
 		return ber0_ps(x,prec);
 	} else {
 		return creal(J0_an(x*csqrt(-I)));
@@ -75,7 +78,7 @@ double ber0(double x,double prec) {
 /* Definiere den Imaginärteil der Besselfunktion 0-ter Ordnung.
  * Nutze dazu eine Fallunterscheidung. */
 double bei0(double x,double prec) {
-	if (x<30) {
+	if (x<10) {
 		return bei0_ps(x,prec);
 	} else {
 		return cimag(J0_an(x*csqrt(-I)));
@@ -84,18 +87,89 @@ double bei0(double x,double prec) {
 
 
 /* Ableitung des Realteils der Besselfunktion 0-ter Ordnung
- * Berechnung durch h-Methode
+ * Berechnung durch 3-Punkt-Methode
  * unter Verwendung der Potenzreihendarstellung */
-double derive_ber0(double x, double prec, double h) {
-	return (ber0(x+h,prec)-ber0(x,prec))/h;
+double derive_ber0(double x, double prec) {
+	const double h = prec;
+	return (ber0(x+h,prec)-ber0(x-h,prec))/h/2;
 }
 
 
 /* Ableitung des Imaginärteils der Besselfunktion 0-ter Ordnung
- * Berechnung durch h-Methode
+ * Berechnung durch 3-Punkt-Methode
  * unter Verwendung der Potenzreihendarstellung */
-double derive_bei0(double x, double prec, double h) {
-	return (bei0(x+h,prec)-bei0(x,prec))/h;
+double derive_bei0(double x, double prec) {
+	const double h = prec;
+	return (bei0(x+h,prec)-bei0(x-h,prec))/h/2;
+}
+
+
+/* * * * * * * * * * Physik  * * * * * * * * */
+
+
+/* Berechne den konstanten Vorfaktor der Stromdichte. 
+double prefactor(double I0, double kappa, double rho0) {
+	return I0*kappa/2*M_1_PI/rho0;
+} */
+
+
+/* Berechne die Stromdichte eines Leiters ohne den konstanten Vorfaktor. */
+double j_1(double kappa, double rho, double rho0, double prec) {
+	return (ber0(kappa*rho,prec)+I*bei0(kappa*rho,prec))/(derive_ber0(kappa*rho0,prec)-I*derive_bei0(kappa*rho0,prec));
+}
+
+
+/* Berechne die Stromdichte eines Leiters.
+ *
+ * I0 - Stromamplitude
+ * rho0 - Radius des Leiters
+ * sigma - Elektrische Leitfähigkeit
+ * mu - Ladungsträgerbeweglichkeit
+ * omega - Kreisfrequenz des Stromes
+ * rho - Abstand von der Drahtachse
+ * prec - Genauigkeit
+ */
+double j(double I0, double rho0, double sigma, double mu, double omega, double rho, double prec) {
+	const double kappa = 2*sqrt(M_PI*mu*sigma*omega)/SI_C;
+	const double prefactor = I0*kappa/2*M_1_PI/rho0;
+
+	return prefactor*j_1(kappa, rho, rho0, prec);
+}
+
+
+/* Berechne für die Kreisfrequenz omega die Verteilung der Stromdichte
+ * über den Leiterquerschnitt. */
+void calculate_current_density_test() {
+	printf("Error: Not implemented.\n");
+}
+
+
+void show_skin_effect() {
+	const double omega = 2*M_PI*5*1E10;    /* 1/s - Kreisfrequenz bei Frequenz f=50MHz. */
+	const double mu = 58.0*1E6;    /* 1/ohm/m - elektrische Leitfähigkeit von Kupfer */
+	const double sigma = 5*1E-3;   /* m*m/V/s - Ladungsträgerbeweglichkeit*/
+	const double rho0 = 1*1E-2;          /* m - Leiterradius */
+	const double I0 = 1;                 /* A - maximale Stromstärke */
+	const double kappa = 2*sqrt(M_PI*mu*sigma*omega)/SI_C;
+	const double prefactor = I0*kappa/2*M_1_PI/rho0;
+
+	const double prec = 1E-8;
+	const double xmin = 0;
+	const double xmax = rho0;
+	const int n = 1000;
+
+	double xi[n+1];
+	double fxi[n+1];
+
+	int i;
+	for(i=0;i<=n; i++){
+		/* Berechne die n-te Stützstelle */
+    		xi[i]=xmin+i*(xmax-xmin)/((double) n);
+		/* Berechne den Funktionswert an der Stützstelle */
+		fxi[i]=prefactor*j_1(kappa,xi[i],rho0,prec);
+    		
+		printf("%f %f \n",xi[i],fxi[i]);
+	}
 }
 
 
@@ -103,25 +177,38 @@ double derive_bei0(double x, double prec, double h) {
 
 double ber0_small_test(double x) { return ber0(x,1E-6); }
 double bei0_small_test(double x) { return bei0(x,1E-6); }
-double derive_ber0_small_test(double x) { return derive_ber0(x,1E-5,1E-8); }
-double derive_ber0_big_test(double x) { return derive_ber0(x,1E-5,1E-8)/exp(x*M_SQRT1_2); }
+double derive_ber0_small_test(double x) { return derive_ber0(x,1E-5); }
+double derive_ber0_big_test(double x) { return derive_ber0(x,1E-5)/exp(x*M_SQRT1_2); }
 double ber0_big_test(double x) { return ber0(x,1E-6)/exp(x*M_SQRT1_2); }
 double bei0_big_test(double x) { return bei0(x,1E-6)/exp(x*M_SQRT1_2); }
 
 
 void plotIt() {
 	plot(&ber0_small_test,0,10,1000); /* Plottet ber(x) */
-//	plot(&bei0_small_test,0,10,1000); /* Plottet ber(x) */
-//	plot(&derive_ber0_small_test,0,10,1000); /* Plottet ber'(x) */
-//	plot(&derive_ber0_big_test,0,100,1000); /* Plottet ber'(x) */
+	plot(&bei0_small_test,0,10,1000); /* Plottet ber(x) */
+	plot(&derive_ber0_small_test,0,10,1000); /* Plottet ber'(x) */
+	plot(&derive_ber0_big_test,0,100,1000); /* Plottet ber'(x) */
 	plot(&ber0_big_test,0,100,1000);/* Plottet J0(x)/exp(x/sqrt(2)) zwischen 0 und 100 */
-//	plot(&bei0_big_test,0,100,1000);/* Plottet J0(x)/exp(x/sqrt(2)) zwischen 0 und 100 */
+	plot(&bei0_big_test,0,100,1000);/* Plottet J0(x)/exp(x/sqrt(2)) zwischen 0 und 100 */
+}
+
+
+void test_continuation () {
+	int i;
+	const double prec = 1E-6;
+
+	printf("ps      as\n");
+	for (i=19;i<22;i++) {
+		printf("%.2f  %.5f  %.5f\n",(double) 0.5*i, (double) ber0_ps(0.5*i,prec),(double) J0_an(0.5*i*csqrt(-I)));
+	}
 }
 
 
 int main(){
-	plotIt();
-	printf("Error: Not Implemented.\n");
+/*	plotIt(); */
+	show_skin_effect();
+/*	printf("Error: Not Implemented.\n"); */
+/*	test_continuation(); */
 
 	return 0;
 }
